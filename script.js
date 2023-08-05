7d18745021495585d02c763d3a93e3021482065b
Joshua Gaviola <martin100906@gmail.com>
2023-08-05 07:50:04 -0700

    creating template

---
+++ script.js
@@ -0,0 +1,15 @@
+// Add your JavaScript code here
+const bird = document.getElementById('bird');
+const gameContainer = document.getElementById('game-container');
+
+// Initialize game state
+
+// Bird jump function
+
+// Pipe creation function
+
+// Game loop function
+
+// Event listeners
+
+// Start the game loop
