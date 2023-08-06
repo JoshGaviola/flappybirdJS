e843b32c31bbbf72fdc958624fa4d994bc5401b0
Joshua Gaviola <martin100906@gmail.com>
2023-08-06 04:19:00 -0700

    added gamestart to wait for user input

--- script.js
+++ script.js
@@ -3,34 +3,34 @@ const gameContainer = document.getElementById('game-container');
 const canvasHeight = gameContainer.clientHeight; // Add this line to get canvas height
 let birdY = 150;
 let birdVelocity = 0;
+let gameStarted = false; // Add a gameStarted flag
 const gravity = 0.5;
 const jumpPower = -10;
-
-// Initialize game state
-birdVelocity += gravity;
-birdY += birdVelocity; // Fix this line to update bird's position
 
 // Bird jump function
 function jump() {
   birdVelocity = jumpPower;
+  gameStarted = true; // Set gameStarted to true after the first jump
 }
 
 // Pipe creation function
 
 // Game loop function
 function gameLoop() {
-  // Update bird's position
-  birdVelocity += gravity;
-  birdY += birdVelocity;
+  if (gameStarted) { // Only update if game has started
+    // Update bird's position
+    birdVelocity += gravity;
+    birdY += birdVelocity;
 
-  // Check for collision with ground or screen top
-  if (birdY > canvasHeight || birdY < 0) {
-    // Handle game over
-    console.log('Game Over');
+    // Check for collision with ground or screen top
+    if (birdY > canvasHeight || birdY < 0) {
+      // Handle game over
+      console.log('Game Over');
+    }
+
+    // Update bird's position on the screen
+    bird.style.transform = `translateY(${birdY}px)`;
   }
-
-  // Update bird's position on the screen
-  bird.style.transform = `translateY(${birdY}px)`;
 
   // Call the game loop again
   requestAnimationFrame(gameLoop);
@@ -41,6 +41,14 @@ document.addEventListener('keydown', (event) => {
   if (event.key === ' ' || event.key === 'ArrowUp') { // Check for both spacebar and up arrow key
     jump();
   }
+});
+
+document.addEventListener('click', () => {
+  jump();
+});
+
+document.addEventListener('touchstart', () => {
+  jump();
 });
 
 // Start the game loop
